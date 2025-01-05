39c1429473158127d1980fd6681839dd82aaad4c
raknage <raknage@raknage.com>
2025-01-05 13:56:40 +0200

    finished

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     path = "./books/frankenstein.txt"
     book_text = load_book_text(path)
-    count_words(book_text)
-    count_chars(book_text)
+    words = count_words(book_text)
+    chars = count_chars(book_text)
+    print_report(words, chars)
 
 
 def load_book_text(path):
@@ -12,21 +13,38 @@ def load_book_text(path):
 
 def count_words(str):
     count = len(str.split())
-    print(count)
     return count
 
 
 def count_chars(str):
     characters = {}
-
     for char in str.lower():
         if char in characters:
             characters[char] += 1
         else:
             characters[char] = 1
-
-    print(characters)
     return characters
+
+
+def convert_to_list(dict):
+    lst = []
+    for char in dict:
+        lst.append({char: dict[char]})
+    lst.sort(key=lambda item: list(item.values())[0], reverse=True)
+    return lst
+
+
+def print_report(word_count, char_dict):
+    list_of_chars = convert_to_list(char_dict)
+
+    print(f"╔═════════ Start report ═════════")
+    print(f"║ Book contains {word_count} words\n║")
+    for item in list_of_chars:
+        char = list(item.keys())[0]
+        count = list(item.values())[0]
+        if char.isalpha():
+            print(f"║  {char} is {count}")
+    print(f"╚══════════ End report ══════════")
 
 
 main()
